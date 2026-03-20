26d2da05876ac263941e19fd64da1a4154bf8d09
Anthony Veloso <anthonyvelosobarral123@outlook.com>
2026-03-20 02:13:46 +0000

    .

--- demo/src/main/mack/lp2/biblioteca/dao/EmprestimoDAO.java
+++ demo/src/main/mack/lp2/biblioteca/dao/EmprestimoDAO.java
@@ -1,5 +1,71 @@
-package mack.lp2.biblioteca.model;
+package mack.lp2.biblioteca.dao;
 
-public class EmprestimoDAO {
-   
+import mack.lp2.biblioteca.model.emprestimo;
+import java.sql.*;
+
+public class EmprestimoDAO extends AbstractDAO {
+
+    @Override
+    public emprestimo create(Object obj) throws SQLException {
+        emprestimo emp = (emprestimo) obj;
+        String sql = "INSERT INTO emprestimo (livro_id, data_retirada) VALUES (?, ?)";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, emp.getLivro_id());
+            stmt.setString(2, emp.getData_retirada());
+            stmt.executeUpdate();
+        }
+        return emp;
+    }
+
+    @Override
+    public emprestimo read(int id) throws SQLException {
+        String sql = "SELECT * FROM emprestimo WHERE id = ?";
+        emprestimo emp = null;
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                emp = new emprestimo(
+                        rs.getInt("livro_id"),
+                        rs.getString("data_retirada")
+                );
+            }
+        }
+        return emp;
+    }
+
+    @Override
+    public emprestimo update(Object obj) throws SQLException {
+        emprestimo emp = (emprestimo) obj;
+        String sql = "UPDATE emprestimo SET livro_id = ?, data_retirada = ? WHERE id = ?";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, emp.getLivro_id());
+            stmt.setString(2, emp.getData_retirada());
+                stmt.setInt(3, emp.getLivro_id());
+            stmt.executeUpdate();
+        }
+        return emp;
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM emprestimo WHERE id = ?";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
 }
--- demo/src/main/mack/lp2/biblioteca/dao/LivroDAO.java
+++ demo/src/main/mack/lp2/biblioteca/dao/LivroDAO.java
@@ -1,25 +1,77 @@
-package mack.lp2.biblioteca.model;
-public class LivroDAO extends AbstractDAO{
-        @Override
-            @Override
-            public  Object create(Object obj) throws SQLException{
-                try(Connection con = super.openConnection()){
-                    private Connection openConnection(){
-                        @Override
-                        public  Object read(int id) throws SQLException{
-                        }
-                        @Override
-                public  Object update(Object obj) throws SQLException{
-                }
-                @Override
-                public  void delete(int id) throws SQLException{
-                }
+package mack.lp2.biblioteca.dao;
 
-                    }
+import mack.lp2.biblioteca.model.Livro;
+import java.sql.*;
 
-            }
+public class LivroDAO extends AbstractDAO {
+
+    @Override
+    public Livro create(Object obj) throws SQLException {
+        Livro livro = (Livro) obj;
+        String sql = "INSERT INTO livro (titulo, autor) VALUES (?, ?)";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setString(1, livro.getNome());
+            stmt.setString(2, livro.getAutor());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                livro.setId(rs.getInt(1));
             }
         }
+        return livro;
+    }
 
+    @Override
+    public Livro read(int id) throws SQLException {
+        String sql = "SELECT * FROM livro WHERE id = ?";
+        Livro livro = null;
 
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
 
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                livro = new Livro(
+                        rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("autor")
+                );
+            }
+        }
+        return livro;
+    }
+
+    @Override
+    public Livro update(Object obj) throws SQLException {
+        Livro livro = (Livro) obj;
+        String sql = "UPDATE livro SET titulo = ?, autor = ? WHERE id = ?";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, livro.getNome());
+            stmt.setString(2, livro.getAutor());
+            stmt.setInt(3, livro.getId());
+            stmt.executeUpdate();
+        }
+        return livro;
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM livro WHERE id = ?";
+
+        try (Connection con = openConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
+}
